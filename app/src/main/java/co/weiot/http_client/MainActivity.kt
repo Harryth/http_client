171a26cba27d6ddd5c1492eff278da1c0c46a8a1
Harold Vallejo <hyvallejom@gmail.com>
2018-06-11 09:19:18 -0500

    feat: Sign up user
    Creates a new user in DB with POST request, authentication through JWT, error handling

--- app/src/main/java/co/weiot/http_client/MainActivity.kt
+++ app/src/main/java/co/weiot/http_client/MainActivity.kt
@@ -1,23 +1,128 @@
 package co.weiot.http_client
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val textView = findViewById<TextView>(R.id.textView)
+
+        // Initialize queue
+        val queue = Volley.newRequestQueue(this)
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //HTTP GET request
+
+//        val url = "https://restserver-206003.appspot.com/user"
+
+//        val stringRequest = StringRequest(Request.Method.GET, url,
+//                Response.Listener<String> { response ->
+//                    android.util.Log.i("HTTP", "Response: $response")
+//
+//                    val jsonResp = JSONObject(response)
+//                    val jsonArray = jsonResp.getJSONArray("user")
+//                    val jsonUser = jsonArray.getJSONObject(0)
+//                    textView.text = "ok: ${jsonResp.getString("ok")}" + "\n" +
+//                            "lastname: ${jsonUser.getString("lastname")}"
+//                }, Response.ErrorListener { error ->
+//            android.util.Log.e("HTTP", error.toString())
+//        })
+
+        //HTTP GET request
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //HTTP POST request for sign up
+
+        val signupUri = "https://restserver-206003.appspot.com/signup"
+        //val testUri = "https://restserver-206003.appspot.com/test"
+
+        // Get data from GUI fields
+        val firstname = findViewById<EditText>(R.id.editFirstName)
+        val lastname = findViewById<EditText>(R.id.editLastName)
+        val username = findViewById<EditText>(R.id.editUsername)
+        val email = findViewById<EditText>(R.id.editMail)
+        val pass = findViewById<EditText>(R.id.editPass)
+
+        // Creates the request
+        val jsonPOSTRequest = object: JsonObjectRequest(Request.Method.POST, signupUri, null,
+                Response.Listener<JSONObject> { res -> // Prints response in console
+
+                    textView.text = "Ok"
+                    Log.d("HTTP", res.toString())
+                },
+                Response.ErrorListener { err -> // Prints error in console
+
+                    val statusCode = err.networkResponse.statusCode // Prints error status code
+                    val errMsg = String(err.networkResponse.data) // Prints error message
+
+                    textView.text = "Fail"
+                    Log.e("HTTP","$statusCode -> $errMsg")
+        }){
+
+            // Set Content-Type to x-www-form-urlencoded
+            override fun getBodyContentType(): String {
+
+                return "application/x-www-form-urlencoded; charset=$paramsEncoding"
+            }
+
+            // Set token to auth platform in the request header
+            override fun getHeaders(): MutableMap<String, String> {
+
+                val params = HashMap<String, String>()
+
+                params.put("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzaWdudXAiLCJpc3MiOiJBbmRyb2lkIEFwcCIsImlhdCI6MTUxNjIzOTAyMn0.tjedUnTGBHqTD2II4OzErDTC-0pUPy1dHca8K58qqr8")
+
+                return params
+            }
+
+            // Set body with encoded params
+            override fun getBody(): ByteArray {
+
+                val body = URLEncoder.encode("firstname", paramsEncoding) + "=" +
+                        URLEncoder.encode("${firstname.text}", paramsEncoding) + "&" +
+                        URLEncoder.encode("lastname", paramsEncoding) + "=" +
+                        URLEncoder.encode("${lastname.text}", paramsEncoding) + "&" +
+                        URLEncoder.encode("username", paramsEncoding) + "=" +
+                        URLEncoder.encode("${username.text}", paramsEncoding) + "&" +
+                        URLEncoder.encode("email", paramsEncoding) + "=" +
+                        URLEncoder.encode("${email.text}", paramsEncoding) + "&" +
+                        URLEncoder.encode("password", paramsEncoding) + "=" +
+                        URLEncoder.encode("${pass.text}", paramsEncoding)
+
+
+                return body.toByteArray()
+            }
+
+        }
+
+        //HTTP POST request for sign up
+        ////////////////////////////////////////////////////////////////////////////////////////////
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+            queue.add(jsonPOSTRequest) // Add request to queue
         }
     }
 
